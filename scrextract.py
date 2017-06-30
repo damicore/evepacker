b12f76ea64132651945d594187d5b422c4222d9b
aware <damiangaggero@gmail.com>
2017-06-30 00:32:34 -0300

    repack() works but everything could use a lot of cleanup

--- scrextract.py
+++ scrextract.py
@@ -47,12 +47,14 @@ def repack(fn):
         file_name = cur_file_rcrd[0:12].rstrip('\x00')
         file_start = unpack('<i', cur_file_rcrd[-4:])[0]
         if found == True:
-            cur_file_rcrd[-4:] = file_start + delta #THIS IS WRONG!!!! DOESNT MODIFY PKD
-            #edit current file record
-            pass
+            print(unpack('<i', pkd[pos+36:pos+40]))
+            print(delta)
+            pkd[pos+36:pos+40] = pack('<i', file_start + delta) #modify the starting_address of the file record
+            print(unpack('<i', pkd[pos+36:pos+40]))
+            continue
         if file_name == fn:
             found = True
-            old_filesize = unpack('<i', cur_file_rcrd[-8:-4])[0]
+            old_file_size = unpack('<i', cur_file_rcrd[-8:-4])[0]
             new_file_size = len(file_to_insert)
             old_file_end = file_start + old_file_size
             delta = new_file_size - old_file_size
@@ -60,14 +62,14 @@ def repack(fn):
             if file_name[-4:] == '.SCR':
                 data = decrypt(data)
             ending_chunk = pkd[old_file_end:] #original ending of file
-            beginning_chunk = pkd[0:file_start]
-            pkd = beginning_chunk.append(data).append(ending_chunk)
-            cur_file_rcrd[-8:-4] = new_file_size #updating filesize
-            pass
-
-
-        fo = open(path + '/' + 'scr.pkd', 'wb') #generalizar con filename
-        fo.write(pkd)
+            beginning_chunk = pkd[:file_start]
+            pkd = beginning_chunk + data + ending_chunk
+            pkd[pos+32:pos+36] = pack('<i', new_file_size) #updating filesize
+            print(pkd[pos+32:pos+36])
+            continue
+    fo = open(path + '/' + 'scr.pkd', 'wb') #generalizar con filename
+    print(path + '/' + 'scr.pkd')
+    fo.write(pkd)
 
 if len(sys.argv) != 2:
     print('Usage: eveunpack [option] <filename>')
